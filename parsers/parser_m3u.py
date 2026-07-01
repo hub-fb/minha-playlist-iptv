0c1b6fddc8c94fa5d5774b8d61f4a7d190c4fbe7
fabianobassetti-dotcom <fabiano.bassetti@gmail.com>
2026-07-01 11:23:16 -0300

    Update parser_m3u.py

--- parsers/parser_m3u.py
+++ parsers/parser_m3u.py
@@ -1,1 +1,24 @@
+def ler_playlist(conteudo):
+    canais = []
 
+    linhas = conteudo.splitlines()
+
+    info = None
+
+    for linha in linhas:
+
+        linha = linha.strip()
+
+        if linha.startswith("#EXTINF"):
+            info = linha
+
+        elif linha.startswith("http://") or linha.startswith("https://"):
+
+            canais.append({
+                "info": info,
+                "url": linha
+            })
+
+            info = None
+
+    return canais
