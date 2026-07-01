4f4e0cf4c9d0fab6e620b6262b08e02787b60b0e
fabianobassetti-dotcom <fabiano.bassetti@gmail.com>
2026-07-01 17:12:27 -0300

    Update parser_json.py

--- parsers/parser_json.py
+++ parsers/parser_json.py
@@ -1,17 +1,39 @@
 import json
 
+
 def ler_json(conteudo):
 
     canais = []
 
     try:
+
         dados = json.loads(conteudo)
 
-        if isinstance(dados, list):
-            for canal in dados:
-                canais.append(canal)
+        if not isinstance(dados, list):
+            return canais
 
-    except:
-        pass
+        for canal in dados:
+
+            nome = canal.get("nome", "Sem nome")
+
+            fontes = canal.get("fontes", {})
+
+            fluxos = fontes.get("fluxos", [])
+
+            if not fluxos:
+                continue
+
+            url = fluxos[0]
+
+            info = f'#EXTINF:-1 group-title="Brasil",{nome}'
+
+            canais.append({
+                "info": info,
+                "url": url
+            })
+
+    except Exception as erro:
+
+        print(f"Erro JSON: {erro}")
 
     return canais
